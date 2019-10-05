597dfd68bb844ec96a348d6476c2581e5e9b6f6c
tgruenewald <terrygruenewald@gmail.com>
2019-10-04 22:29:42 -0600

    add standard assets and basic character

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewBehaviourScript : MonoBehaviour {
+
+	public void start_the_game() {
+		SceneManager.LoadScene("scene1");
+	}
+}
